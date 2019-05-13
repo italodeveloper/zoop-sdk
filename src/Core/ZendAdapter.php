1b45470be5e6266f5fb8621d3ebe79e62d1dc765
Ítalo Araújo <40692842+italodeveloper@users.noreply.github.com>
2019-05-13 15:05:30 -0300

    Correção e documentação do ZendAdapter Zoop

--- src/Core/ZendAdapter.php
+++ src/Core/ZendAdapter.php
@@ -1,16 +1,41 @@
 <?php
 namespace Zoop\Core;
 
+/**
+ * ZendAdapter class
+ * 
+ * Essa classe realiza em formato Fluente assum como o Guzzle
+ * a adaptação das requests realizadas com Guzzle para o Zend Http
+ * utilziando as PSR'S corretamentes isso fica simples e eficaz.
+ * 
+ * @package Zoop\Core
+ * @author italodeveloper <italoaraujo788@gmail.com>
+ * @version 1.0.0
+ */
 class ZendAdapter 
 {
+    /** @var $guzzle */
     protected $guzzle;
     public function __construct(array $guzzle)
     {
         $this->guzzle = $guzzle;
     }  
     
+    /**
+     * request function
+     *
+     * Simula a request do Guzzle utilizando o Zend Http
+     * 
+     * @param string $method
+     * @param string $url
+     * @param [type] $body
+     * @return $this
+     */
     public function request(string $method, string $url, $body = null)
     {
+        if(is_null($body)){
+            $body = [];
+        }
         $method = strtoupper($method);
         if(!is_array($body)){
             return false;
@@ -32,11 +57,27 @@ class ZendAdapter
         return $this;
     }
 
+    /**
+     * getBody function
+     *
+     * Existe para garantir a continuidade e fluidez das chamadas
+     * 
+     * @return void
+     */
     public function getBody()
     {
         return $this;
     }
 
+    /**
+     * getContents function
+     *
+     * Pega todos os dados fluidos realiza as verificações bases,
+     * como se a zoop retornou algum error, se sim cria a exception
+     * 
+     * @return string
+     * @throws Exception
+     */
     public function getContents()
     {
         $properties = get_object_vars($this);
